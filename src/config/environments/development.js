e3e8a19db9a77e6e5d3c2075808278f88ba29f6d
Francisco Rios <panchorios_242@hotmail.com>
2022-01-22 01:50:17 -0300

    Fixed package.json version && added libraries

---
+++ src/config/environments/development.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PORT: 5000,
+  DB: {
+    username: "root",
+    password: "root",
+    database: "ong-dev",
+    host: "localhost",
+    port: "3306",
+    dialect: "mysql",
+  },
+};
---
+++ src/config/environments/production.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+  PORT: process.env.SERVER_PORT,
+  DB: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+};
